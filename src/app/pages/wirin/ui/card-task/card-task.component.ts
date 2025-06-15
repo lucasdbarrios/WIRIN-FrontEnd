5a0a596c45de7396263c8e526202710c944bdd84
Lucas Barrios <lucabarrios@alumno.unlam.edu.ar>
2025-06-15 02:50:55 -0300

    priority color

--- src/app/pages/wirin/ui/card-task/card-task.component.ts
+++ src/app/pages/wirin/ui/card-task/card-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { getSeverity } from '../../../../utils/getSeverity';
 import { ButtonModule } from 'primeng/button';
@@ -15,13 +15,37 @@ import { FormsModule } from '@angular/forms';
     ]
 })
 
-export class CardTaskComponent {
+export class CardTaskComponent implements OnInit {
     @Input() task: any;
     @Input() showCheckbox: boolean = false;
     @Output() taskSelectionChange = new EventEmitter<boolean>();
     @Output() showTaskDetail = new EventEmitter<number>();
-
+    
+    ngOnInit() {
+        this.checkDueDate();
+    }
+    
     getSeverity(status: string): string {
         return getSeverity(status);
+    }
+    
+    checkDueDate() {
+        if (this.task && this.task.limitDate) {
+            const today = new Date();
+            const limitDate = new Date(this.task.limitDate);
+            
+            // Resetear las horas para comparar solo fechas
+            today.setHours(0, 0, 0, 0);
+            limitDate.setHours(0, 0, 0, 0);
+            
+            // Calcular la diferencia en días
+            const diffTime = limitDate.getTime() - today.getTime();
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            
+            // Si falta 1 día o menos, establecer como prioridad alta
+            if (diffDays <= 1) {
+                this.task.isPriority = true;
+            }
+        }
     }
 }
