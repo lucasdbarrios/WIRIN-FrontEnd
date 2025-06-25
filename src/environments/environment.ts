178fbc1e44698c47945d2e61b5956b59b012d0f2
Tomas <talk.gtg@gmail.com>
2025-06-24 22:05:13 -0300

    url api test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // La lista de reemplazos de archivos se puede encontrar en angular.json.
 
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:44342/api'
+    production: true,
+    apiUrl: 'https://api.wirinadapta.com.ar/api'
   };
