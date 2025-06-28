f637abef44792c4f63df24abde8c2b240b13d391
Lucas Barrios <lucabarrios@alumno.unlam.edu.ar>
2025-06-28 13:12:37 -0300

    visible ranking to volunteer

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -35,7 +35,7 @@ export class AppMenu {
                     ...(this.isBibliotecario ? [{ label: 'Dashboard', icon: 'pi pi-briefcase', routerLink: ['/wirin/dashboard'] }] : []),
                         { label: 'Tareas', icon: 'pi pi-fw pi-list', routerLink: ['/wirin/tasks'] },
                     ...(this.isBibliotecario ? [{ label: 'Usuarios', icon: 'pi pi-fw pi-users', routerLink: ['/wirin/users'] }] : []),
-                    ...(this.isBibliotecario ? [{ label: 'Ranking Voluntarios', icon: 'pi pi-fw pi-chart-line', routerLink: ['/wirin/volunteer-ranking'] }] : []),
+                    ...(this.isBibliotecario || this.isVoluntario ? [{ label: 'Ranking Voluntarios', icon: 'pi pi-fw pi-chart-line', routerLink: ['/wirin/volunteer-ranking'] }] : []),
                     ...(this.isVoluntario ? [{ label: 'Mis tareas', icon: 'pi pi-fw pi-clipboard', routerLink: ['/wirin/tasks-voluntario'] }] : []),
                     ...(this.isVoluntario ? [{ label: 'Mis Estadísticas', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/wirin/volunteer-stats'] }] : []),
                     // ...(this.isBibliotecario ? [{ label: 'Entregas', icon: 'pi pi-fw pi-send', routerLink: ['/wirin/deliveries'] }] : []),
