87b306b78ecbd4c39206d82e7b638b06469eaabc
martin <guerreiromartin@gmail.com>
2025-06-29 14:04:10 -0300

    styles of ranking view changed

--- src/app/pages/wirin/ui/data-task/data-task.component.ts
+++ src/app/pages/wirin/ui/data-task/data-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
 
 
 @Component({
     selector: 'app-data-task',
     templateUrl: './data-task.component.html',
-    imports: [CommonModule],
+    imports: [CommonModule, CardModule],
 })
 
 export class DataTaskComponent {
